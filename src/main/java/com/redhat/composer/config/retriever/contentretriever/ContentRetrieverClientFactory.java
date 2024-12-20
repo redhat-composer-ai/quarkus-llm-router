00930b33f956657bf9ea890fe12ca9bda6905804
bala <bala@redhat.com>
2024-12-20 10:41:16 +0800

    Making the vector store selection configurable

--- src/main/java/com/redhat/composer/config/retriever/contentretriever/ContentRetrieverClientFactory.java
+++ src/main/java/com/redhat/composer/config/retriever/contentretriever/ContentRetrieverClientFactory.java
@@ -5,6 +5,8 @@ import com.redhat.composer.model.enums.ContentRetrieverType;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 /**
  * Factory for Content Retriever Clients.
  */
@@ -13,17 +15,21 @@ public class ContentRetrieverClientFactory {
 
   @Inject
   WeaviateContentRetrieverClient weaviateEmbeddingStoreClient;
-  
+
   @Inject
   Neo4jContentRetrieverClient neo4jContentRetrieverClient;
 
   @Inject
   ElasticsearchContentRetrieverClient elasticsearchContentRetrieverClient;
-  
-  static final ContentRetrieverType DEFAULT_CONTENT_RETRIEVER = ContentRetrieverType.WEAVIATE;
+
+  @ConfigProperty(name = "vector.store.type")
+  static String contentRetrieverType;
+
+  static final ContentRetrieverType DEFAULT_CONTENT_RETRIEVER = ContentRetrieverType.fromString(contentRetrieverType);
 
   /**
    * Get the Content Retriever Client.
+   * 
    * @param contentRetrieverType the ContentRetrieverType
    * @return the Content Retriever Client
    */
@@ -31,17 +37,17 @@ public class ContentRetrieverClientFactory {
     if (contentRetrieverType == null) {
       contentRetrieverType = DEFAULT_CONTENT_RETRIEVER;
     }
-    
+
     switch (contentRetrieverType) {
-      case ContentRetrieverType.WEAVIATE:
+      case WEAVIATE:
         return weaviateEmbeddingStoreClient;
-      case ContentRetrieverType.NEO4J:
+      case NEO4J:
         return neo4jContentRetrieverClient;
-      case ContentRetrieverType.ELASTICSEARCH:
+      case ELASTICSEARCH:
         return elasticsearchContentRetrieverClient;
       default:
         throw new RuntimeException("Content Retriever type not found: " + contentRetrieverType);
     }
   }
-  
+
 }
